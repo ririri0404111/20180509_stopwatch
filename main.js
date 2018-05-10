5d4310072a0c557c239afb3872b8ed9a61c30d96
yuri <lily0114@gmail.com>
2018-05-10 15:53:09 +0900

    ver.0.1

---
+++ main.js
@@ -0,0 +1,40 @@
+(function (){
+  'use strict';
+  window.addEventListener('load',function(){
+
+    var timer = document.getElementById('timer');
+    console.log('timer',timer);
+
+    document.getElementById('start');
+    var start = document.getElementById('start');
+    var stop = document.getElementById('stop');
+    var reset = document.getElementById('reset');
+    var startTime;
+    var progressTime;
+
+
+
+    function update () {
+      var elapsedTime;
+      elapsedTime = (Date.now() - startTime) / 1000;
+      timer.textContent = elapsedTime;
+    }
+
+    start.addEventListener('click',function(){
+      startTime = Date.now();
+      progressTime = setInterval(update, 30);
+    });
+
+    stop.addEventListener('click',function(){
+      var stopTime;
+      clearInterval(progressTime);
+      stopTime = (Date.now() - startTime) / 1000;
+      timer.textContent = stopTime;
+    });
+
+    reset.addEventListener('click',function(){
+      timer.textContent = '00:00.000';
+;    })
+
+  });
+})();
